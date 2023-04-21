966e0d80c5ef62dde1ed3bad3cc6bf304e0369e3
JezreelCircles <chisomorehemagondwe@gmail.com>
2023-04-21 15:07:05 -0700

    fifth commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - prints all single digit numbers
+* Return: Always 0
+*/
+int main(void)
+{
+int numbers;
+for (numbers = 0; numbers < 10; numbers++)
+	;
+printf("%i", numbers);
+printf("\n");
+return (0);
+}
