8de551fc4dd725a6378a2d583348ce617290afe8
JezreelCircles <chisomorehemagondwe@gmail.com>
2023-04-22 08:26:20 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 /**
-* main - prints all single digit numbers
-* Return: Always 0
-*/
+ * main - print numbers and a new line
+ * Return: Success 0
+ */
 int main(void)
 {
-int numbers;
-for (numbers = 0; numbers < 10; numbers++)
-;
-printf("%d", numbers);
-printf("\n");
+int i;
+for (i = 0; i < 10; i++)
+{
+printf("%d", i)
+}
+putchar('\n');
 return (0);
 }
--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
-* main - print single digits in base 10
-* Return: Always 0
+* main - Entry point
+* Return: Always 0 (Success)
 */
 int main(void)
 {
-int numbers;
-for (numbers = 0; numbers < 10' numbers++)
-putchar((numbers % 10) + '0');
-putchar('\n');
+int d;
+for (d = '0'; <= '9'; d++)
+putchar(d);
+putchar(\n');
 return (0);
-}
+}	
