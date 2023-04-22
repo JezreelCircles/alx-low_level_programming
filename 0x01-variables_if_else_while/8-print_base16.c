df7b9de2f75b389f95ed00ed7547978cf0bd444c
JezreelCircles <chisomorehemagondwe@gmail.com>
2023-04-22 09:45:23 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int d;
+char lowercase;
+
+for (d = '0'; d <= '9'; d++)
+putchar(d);
+
+for (lowercase = 'a'; lowercase <= 'f'; lowercase++)
+putchar(lowercase);
+putchar('\n');
+
+return (0);
+}
