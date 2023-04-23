8801e218f3ee84aaee451d54e4bfa8a47d387eaf
JezreelCircles <chisomorehemagondwe@gmail.com>
2023-04-23 10:59:32 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,17 +17,17 @@ for (p = 0; p <= 98; p++)
 	{
 
 for (q = p + 1; q <= 99; q++)
-		{
-			putchar((p / 10) + '0');
-			putchar((p % 10) + '0');
-			putchar(' ');
-			putchar((q / 10) + '0');
-			putchar((q % 10) + '0');
-			if (p == 98 && q == 99)
-			continue;
-			putchar(',');
-			putchar(' ');
-		}
+	{
+	putchar((p / 10) + '0');
+	putchar((p % 10) + '0');
+	putchar(' ');
+	putchar((q / 10) + '0');
+	putchar((q % 10) + '0');
+	if (p == 98 && q == 99)
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
 	}
 	putchar('\n');
 	return (0);
