df8a0174db4b9b6b2b7aee3836d0180961c97608
JezreelCircles <chisomorehemagondwe@gmail.com>
2023-04-25 17:07:14 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * print alphabet - Entry point
+ * Return: Always 0 upon success
+ */
+void print_alphabet(void);
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+
+{
+		_putchar(c);
+	}
+	_putchar('\n');
+	return (0);
+}
