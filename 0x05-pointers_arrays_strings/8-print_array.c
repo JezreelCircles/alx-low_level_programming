8c87e1889cae3c0f4090321b42744bee2e180662
JezreelCircles <chisomorehemagondwe@gmail.com>
2023-05-03 07:46:46 -0700

    task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,18 +13,17 @@
 
 void print_array(int *a, int n)
 {
+int index;
 
-int i;
-
-for (i = 0; i < (n - 1); i++)
+for (index = 0; index < n; index++)
 {
-printf("%d", a[i]);
-}
-if (i == (n - 1))
+printf("%d", a[index]);
+if (index != (n - 1))
 {
-printf("%d", a[n - 1]);
+printf(" , ");
+}
 }
 
-putchar('\n');
+printf("\n");
 
 }
