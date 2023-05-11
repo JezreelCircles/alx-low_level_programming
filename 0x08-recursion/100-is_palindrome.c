6d7ae29a725642db7a9f792936751f2d637f4224
JezreelCircles <chisomorehemagondwe@gmail.com>
2023-05-11 04:11:18 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -2,11 +2,11 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - returns the length of a string.
- * @s: string.
- * return: 1 if a string is a palindrome and 0 if not.
- *
+ * _strlen_recursion - returns the length of a string
+ * @s: string
+ * Return: the length of the string
  */
+
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
@@ -17,34 +17,33 @@ int _strlen_recursion(char *s)
 }
 
 /**
- * comparator - comcompares each character of the string.
- * @s: string.
- * @n1: smallest iterator.
- * @n2: biggest iterator.
- * return: 1 if s is a palindrome, otherwise return 0.
+ * comparator - compares each character of the string
+ * @s: string
+ * @n1: smallest iterator
+ * @n2: biggest iterator
+ * Return: 1 if s is a palindrome, 0 otherwise
  */
 
-int comparator(char *s, int n1, int n2)
+int comparator(char s, int n1, int n2)
 {
-	if (*(s + n1) == *(s + n2))
-	{
-		if (n1 == n2 || n1 == n2 + 1)
-			return (1);
-		return (0 + comparator(s, n1 + 1, n2 - 1));
-	}
-	return (0);
+if ((s + n1) == *(s + n2))
+{
+if (n1 == n2 || n1 == n2 + 1)
+return (1);
+return (0 + comparator(s, n1 + 1, n2 - 1));
+}
+return (0);
 }
 
 /**
- * is_palindrome - detects if a string is a palindrome.
- * @s: string.
- * return: 1 if s is a palindrome, otherwise return 0.
- *
+ * is_palindrome - detects if a string is a palindrome
+ * @s: string
+ * Return: 1 if s is a palindrome, 0 otherwise
  */
 
 int is_palindrome(char *s)
 {
-	if (*s == '\0')
-		return (1);
-	return (comparator(s, 0, _strlen_recursion(s) - 1));
+if (*s == '\0')
+return (1);
+return (comparator(s, 0, _strlen_recursion(s) - 1));
 }
