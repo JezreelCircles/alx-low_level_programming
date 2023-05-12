480b143c839c50dab4644fe6c61a1fcb7c759b8a
JezreelCircles <chisomorehemagondwe@gmail.com>
2023-05-12 13:10:54 -0700

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,9 +9,11 @@
  * Return: 0 (success)
  */
 
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
+
 {
-	(void) argc;
-	printf("$s\n", argv[0]);
+
+	printf("%s\n", *argv);
+
 	return (0);
 }
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -3,6 +3,7 @@
 
 int _putchar(char c);
 int main (int argc, char *argv[]);
-
+int _atoi(char *s);
+int main(int argc __attribute__((unused)), char *argv[]);
 
 #endif
