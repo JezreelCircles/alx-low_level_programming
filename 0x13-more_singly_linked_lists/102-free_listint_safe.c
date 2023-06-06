f5f8f641950fa2d221ef4d536a13fcef6b206e35
JezreelCircles <chisomorehemagondwe@gmail.com>
2023-06-06 15:27:20 -0700

    Free safe version

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -13,7 +13,7 @@ size_t free_listint_safe(listint_t **h)
 {
 
 size_t len = 0;
-int d;
+int diff;
 listint_t *temp;
 
 if (!h || !*h)
@@ -21,8 +21,8 @@ return (0);
 
 while (*h)
 {
-d = *h - (*h)->next;
-if (d > 0)
+diff = *h - (*h)->next;
+if (diff > 0)
 {
 temp = (*h)->next;
 free(*h);
