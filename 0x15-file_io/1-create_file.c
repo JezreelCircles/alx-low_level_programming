271b336427c64ec57a84174f38a0a1b16344a555
JezreelCircles <chisomorehemagondwe@gmail.com>
2023-06-13 15:00:21 -0700

    task one correction

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,8 +17,8 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (fd == -1)
-			return (-1);
+if (fd == -1)
+return (-1);
 	if (!text_content)
 		text_content = "";
 
@@ -28,7 +28,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (permissions == -1)
 		return (-1);
-	close (fd);
+	close(fd);
 
 	return (1);
 }
