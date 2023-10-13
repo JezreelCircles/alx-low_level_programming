1fe83b6a70fbcd88982f9db85242c389d99ab44e
JezreelCircles <chisomorehemagondwe@gmail.com>
2023-10-13 16:34:43 -0700

    sco commit

--- 0x17-doubly_linked_lists/3-main.c
+++ 0x17-doubly_linked_lists/3-main.c
@@ -19,8 +19,8 @@ int main(void)
     add_dnodeint_end(&head, 3);
     add_dnodeint_end(&head, 4);
     add_dnodeint_end(&head, 98);
-    add_dnodeint_end(&head, 402);
-    add_dnodeint_end(&head, 1024);
-    print_dlistint(head);
-    return (EXIT_SUCCESS);
+add_dnodeint_end(&head, 402);
+add_dnodeint_end(&head, 1024);
+print_dlistint(head);
+return (EXIT_SUCCESS);
 }
